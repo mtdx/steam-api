85e4dd40b7391494e12adef7e62493c9646f3ae9
mtdx <ms2asdk@gmail.com>
2017-07-21 09:22:53 -0700

    lint seed files

--- seeds/01_users.js
+++ seeds/01_users.js
@@ -1,7 +1,7 @@
 
 exports.seed = knex => {
   return knex('users').del()
-    .then(function () {
+    .then(() => {
       return knex('users').insert([
         { id: 1, username: 'admin', password: 'e3TPs9aSShhRwG3B', scope: 'u' } // default pass
       ]);
--- seeds/02_steam_groups.js
+++ seeds/02_steam_groups.js
@@ -1,7 +1,7 @@
 
 exports.seed = knex => {
   return knex('steam_groups').del()
-    .then(function () {
+    .then(() => {
       return knex('steam_groups').insert([
         { group_link: 'csgofast', user_id: 1 },
         { group_link: 'gostrongcom', user_id: 1 },
