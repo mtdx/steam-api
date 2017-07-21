7eb08a4a554fe5fb89573e9e42a0eec57b358115
mtdx <ms2asdk@gmail.com>
2017-07-21 01:02:45 -0700

    and user domain and service

---
+++ service/UserService.ts
@@ -0,0 +1,19 @@
+import * as Logger from 'bunyan';
+import { User } from '../domain/User';
+import { IDatabase } from 'pg-promise';
+
+export class UserService {
+    protected _log: Logger;
+
+    constructor(protected _db: IDatabase<any>, _log: Logger) {
+        this._log = _log.child({ service: 'UserService' });
+    }
+
+    async findByUsernameAndPassword(username: string, password: string): Promise<User> {
+        this._log.info('Request to get user by username and password.');
+        return this._db.oneOrNone(
+            'SELECT id, username, scope FROM users WHERE username = $1 AND password = $2',
+            [username, password]
+        );
+    }
+}
--- web/rest/auth.ts
+++ web/rest/auth.ts
@@ -1,15 +1,13 @@
+import { UserService } from '../../service/userService';
 import * as jwt from 'jsonwebtoken';
 import * as Joi from 'joi';
 import { db } from '../../common/db';
 import { TokenSchema, ErrorSchema } from '../../schemas/shared';
+import { User } from '../../domain/User';
+import * as bunyan from 'bunyan';
 
 const privateKey = process.env.STEAMAPP_TOKEN_SIGNING_KEY || 'InsecurePrivateKey';
-
-const getUserFromDb = async (username: string, password: string) =>
-    db.oneOrNone(
-        'SELECT id, scope FROM users WHERE username = $1 AND password = $2',
-        [username, password]
-    );
+const log = bunyan.createLogger({ name: 'auth' });
 
 export const auth = [
     {
@@ -24,7 +22,8 @@ export const auth = [
             },
             handler: async (request, reply) => {
                 // obviously password should be salted and hashed but for personal use is OK.
-                const user = await getUserFromDb(request.payload.username, request.payload.password);
+                const user: User = await (new UserService(db, log))
+                .findByUsernameAndPassword(request.payload.username, request.payload.password);
                 if (!user) {
                     reply({ statusCode: 401, error: 'Invalid Username or Password' });
                     return;
