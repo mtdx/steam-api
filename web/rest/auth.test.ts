ee81be9fa0b504f957ecadb0d71c61c2799e4a67
mtdx <ms2asdk@gmail.com>
2017-07-21 07:05:06 -0700

    add auth tests

--- web/rest/auth.test.ts
+++ web/rest/auth.test.ts
@@ -1,13 +1,70 @@
 import 'jest';
 import { server } from '../../server';
+import * as Joi from 'joi';
+import { ErrorSchema, TokenSchema } from '../../common/schema';
 
-describe('Basic HTTP /auth route', () => {
-  it('returns 401 HTTP status code', done => {
-    const options = { method: 'POST', url: '/auth' };
+describe('/auth route & / login', () => {
+  let auth: any;
+
+  beforeAll(async done => {
+    setTimeout(() => {
+      done();
+    }, 20);
+  });
+
+  beforeAll(async done => {
+    const options = {
+      method: 'POST',
+      url: '/auth',
+      payload: {
+        username: 'admin',
+        password: 'e3TPs9aSShhRwG3B'
+      }
+    };
     server.inject(options, response => {
-      expect(response.statusCode).toBe(401);
-      server.stop();
+      auth = Joi.validate(response.result, TokenSchema);
+      expect(response.statusCode).toBe(200);
+      expect(auth.error).toBeNull();
+      expect(auth.value.statusCode).toBe(200);
+      expect(auth.value.token).toBeDefined();
       done();
     });
+  });
+
+  it('POST /auth & fails login -> 401', async done => {
+    const options = {
+      method: 'POST',
+      url: '/auth',
+      payload: {
+        username: 'fakeadmin',
+        password: 'fakepassword'
+      },
+    };
+    server.inject(options, response => {
+      expect(response.statusCode).toBe(401);
+      const error: any = Joi.validate(response.result, ErrorSchema);
+      expect(error.error).toBeNull();
+      expect(error.value.statusCode).toBe(401);
+      expect(error.value.error).toBe('Authentication Failed');
+      expect(error.value.message).toBe('Invalid Username or Password');
+      done();
+    });
+  });
+
+  it('GET / & success login -> 200', async done => {
+    const options = {
+      method: 'GET',
+      url: '/',
+      headers: { Authorization: 'Bearer ' + auth.value.token }
+    };
+    server.inject(options, response => {
+      expect(response.statusCode).toBe(200);
+      done();
+    });
+  });
+
+  afterAll(async done => {
+    server.stop();
+    done();
   });
 });
